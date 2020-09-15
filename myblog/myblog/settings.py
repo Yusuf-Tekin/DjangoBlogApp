4a905c1f48e50a309c8d3cccd69f488cc36242ea
yusuf.tekinx <59294270+Yusuf-Tekin@users.noreply.github.com>
2020-09-15 17:21:24 +0300

    settings.py değiştirildi

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -114,3 +114,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
